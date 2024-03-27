a66e1f610dc27f7a3d8ed7e74449a40802d46ff1
AshleyDafneAguilar <119641994+AshleyDafneAguilar@users.noreply.github.com>
2024-03-26 21:36:41 -0600

    Update code.py

--- code.py
+++ code.py
@@ -3,26 +3,24 @@
 #Ashley Dafne Aguilar Salinas
 
 
-"""# Librerías
+"""## Librerías
 """
-
+!pip install contextily
 import pandas as pd
 import geopandas as gpd
+from pandas import ExcelFile
 
 import openpyxl
 import contextily
 
-from pandas import ExcelFile
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-# # Función
-
-# In[2]:
-
-
+"""## Función
+"""
+#Permite agregar un mapa base de alta calidad 
+#en el fondo de un gráfico de datos geoespaciales
+#Esta función utiliza el servicio web de mapas base(OSM).
 def on_basemap(ax):
     ax.set_aspect('equal')
     contextily.add_basemap(
@@ -30,232 +28,111 @@ def on_basemap(ax):
       source=contextily.providers.OpenStreetMap.Mapnik
     )
     return ax
+# ---
 
+"""## Lectura del archivo .xlsx
+"""
 
-# # Lectura del DataFrame
-
-# In[3]:
-
-
-PATH = './data/'
-
-
-# In[4]:
-
-
+PATH = './dataset/'
 xlsx = pd.ExcelFile(PATH + "Identificación de Problemáticas.xlsx", engine='openpyxl')
-
-
-# In[5]:
-
-
 df = xlsx.parse(sheet_name='Casos')
 
-
-# In[6]:
-
-
+#Mostramos el DataFrame
 df
 
-
 # ---
-
-# # Limpieza
-
-#  Nulos
-
-# In[7]:
+"""## Limpieza
 
 
+###  Valores Nulos
+"""
+
+# Visualizamos los valores nulos
 df.iloc[24:37]
 
-
-# In[8]:
-
-
+#Borramos los valores nulos 
+#de nuestro DataFrame
 df.drop([x for x in range(24, 38, 1)], inplace=True)
-
-
-# In[9]:
-
-
 df.reset_index(drop = True, inplace = True)
 
-
-# In[10]:
-
-
+#Mostramos los cambios
 df
-
-
 # ---
 
-# # Caminos
-
-# In[11]:
-
-
+"""## Caminos
+"""
+#Obtenemos todos los datos que
+#son indicados como caminos 
 df[df['Notas'].str.contains('Camino') == True]
-
-
 # ---
 
-# # Puntos
-
-# In[12]:
-
-
+"""## Puntos
+"""
+#Obtenemos todos los datos que
+#no son indicados como caminos
+#y los almacenamos como puntos
 df_puntos = df.drop(df.index[df['Notas'].str.contains('Camino') == True])
 
-
-# In[13]:
-
-
+#Mostramos los datos
+#indicados como puntos
 df_puntos
-
-
 # ---
 
-# # Graficar
-
-# In[14]:
-
-
-#se elimina el último dato porque no hay información
-
+"""## Graficar
+"""
+#Convertimos el DataFrame de 
+#los datos marcados como puntos 
+#a un Geopandas.Se elimina el 
+#último dato porque no hay información
 geodf_puntos =  gpd.GeoDataFrame(df_puntos.drop([32]))
-
-
-# In[15]:
-
-
 geodf_puntos.reset_index(drop = True, inplace = True)
 geodf_puntos
 
-
-# In[16]:
-
-
+#convertimos las columnas de Longitud y
+#Latitud en un solo atributo de tipo POINT
 gpd.GeoDataFrame(geometry=gpd.points_from_xy(geodf_puntos['Longitud'], geodf_puntos['Latitud']))
 
-
-# In[17]:
-
-
+#Almacenamos ese cambio en un nuevo
+#atributo denominado geometry
 geodf_puntos['geometry'] = gpd.GeoDataFrame(geometry=gpd.points_from_xy(geodf_puntos['Latitud'], geodf_puntos['Longitud']))
 
-
-# In[18]:
-
-
+#Mostramos los cambios 
+#hechos al GeoDataFrame
 geodf_puntos
+# ---
 
 
-# ## Coordenadas
+"""## Coordenadas
+"""
 
-# In[19]:
-
-
+# Para obtener el CRS actual de un 
+#GeoDataFrame, se puede utilizar la 
+#función .crs sin argumentos.
 geodf_puntos.crs
 
-
-# In[20]:
-
-
+#Cambiar el sistema de referencia de 
+#coordenadas(CRS) de un GeoDataFrame
+#EPSG 4326 es empleado para la 
+#representación de la cartografía a nivel mundial.
 geodf_puntos = geodf_puntos.set_crs("EPSG:4326") 
 
-
-# In[21]:
-
-
+#Transformamos las coordenadas a ESPDG 3857 
+#que es el que utilizan la mayoría de los 
+#mapas representados en la web
 geodf_puntos = geodf_puntos.to_crs(epsg=3857)
+# ---
 
 
-# In[22]:
-
-
+#Ploteamos los puntos de nuestro 
+#GepoDataFrame en el mapa
 on_basemap(geodf_puntos.to_crs(epsg=3857).plot(figsize=(30,30)))
-
-
-# In[23]:
-
-
 on_basemap(geodf_puntos.plot())
 
 
-# In[24]:
-
-
+#Visualiza los puntos fuera del mapa
 #geodf_puntos.explore(legend=False)
-
-
-# In[25]:
-
-
 geodf_puntos['geometry'].plot()
 
-
 # ---
-
-# ---
-
-# ## AGEBS
-
-# In[7]:
-
-
-PATh = './mapping/'
-agebs = gpd.read_file(PATh + 'conjunto de datos/16a.shp')
-
-
-# In[8]:
-
-
-agebs
-
-
-# In[58]:
-
-
-# Morelia
-agebs_Morelia = agebs[agebs['CVE_MUN'] == '053'].reset_index(drop=True)
-agebs_Morelia
-
-
-# In[59]:
-
-
-agebs_Morelia = agebs_Morelia.to_crs(epsg=6369)
-
-
-# In[60]:
-
-
-agebs_Morelia.plot()
-plt.title('Zonas AGEB en Morelia')
-plt.xlabel('Longitud')
-plt.ylabel('Latitud')
-plt.show()
-
-
-# In[62]:
-
-
-# La proyección que utilizamos no es compatible aún con contextily entonces usamos otra temporalmente
-# Sólo para hacer la visualización en el mapa
-on_basemap(agebs_Morelia.to_crs(epsg=3857).plot(figsize=(50,50),alpha=0.5))
-plt.title('Mapa de Morelia con AGEBs')
-plt.show()
-
-
-# In[67]:
-
-
-on_basemap(geodf_puntos['geometry'].plot())
-
-
-# In[ ]:
-
-
 
 
